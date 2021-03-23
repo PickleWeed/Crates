1d3f5a60ede80287a99bfaab7dc7a29c223b9bad
ivanchay98 <ivanchay98@gmail.com>
2021-03-23 20:24:22 +0800

    Update RetrieveListingTest.dart

--- project_crates/lib/DatabaseTesting/RetrieveListingTest.dart
+++ project_crates/lib/DatabaseTesting/RetrieveListingTest.dart
@@ -1,5 +1,6 @@
 import 'package:firebase_database/firebase_database.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_application_1/models/Listing.dart';
 import 'package:flutter_application_1/services/databaseAccess.dart';
 
 class RetrieveListingTest extends StatefulWidget {
@@ -25,7 +26,8 @@ class _RetrieveListingTestState extends State<RetrieveListingTest> {
               DataSnapshot dataValues = snapshot.data.snapshot;
               Map<dynamic, dynamic> values = dataValues.value;
               values.forEach((key, value) {
-                listingList.add(value);
+                Listing listing = dbToListing(value);
+                listingList.add(listing);
               });
               return new ListView.builder(
                 shrinkWrap: true,
@@ -35,14 +37,13 @@ class _RetrieveListingTestState extends State<RetrieveListingTest> {
                       child: Column(
                     crossAxisAlignment: CrossAxisAlignment.start,
                     children: [
-                      Text("Title: " + listingList[index]["listingTitle"]),
-                      Text("Description: " + listingList[index]["description"]),
-                      Text("Category: " + listingList[index]["category"]),
+                      Text("Title: " + listingList[index].listingTitle),
+                      Text("Description: " + listingList[index].description),
+                      Text("Category: " + listingList[index].category),
                       Row(
-                          children: listingList[index]["listingImage"] == null
-                              ? [Container(height: 50, width: 50)]
-                              : urlToList(listingList[index]["listingImage"]
-                                  .split('||'))),
+                          children: listingList[index].listingImage.length != 0
+                              ? urlToList(listingList[index].listingImage)
+                              : [Container(height: 50, width: 50)]),
                     ],
                   ));
                 },
@@ -53,11 +54,32 @@ class _RetrieveListingTestState extends State<RetrieveListingTest> {
   }
 
   List<Widget> urlToList(List<String> imageURLs) {
-    imageURLs.removeLast();
+    // imageURLs.removeLast();
     List<Widget> widgetList = [];
     for (var url in imageURLs) {
       widgetList.add(Image.network(url, width: 50, height: 50));
     }
     return widgetList;
+  }
+
+  Listing dbToListing(Map databaseEntry) {
+    List<String> imageURLs = databaseEntry["listingImage"] != null
+        ? databaseEntry["listingImage"].split('||')
+        : ['dummy'];
+    imageURLs.removeLast();
+
+    Listing listing = new Listing(
+      isRequest: databaseEntry["isRequest"],
+      category: databaseEntry["category"],
+      listingTitle: databaseEntry["listingTitle"],
+      description: databaseEntry["description"],
+      // postDateTime: DateTime.parse(databaseEntry["postDateTime"]),
+      userID: databaseEntry["userID"],
+      latitude: databaseEntry["latitude"],
+      longitude: databaseEntry["longitude"],
+      listingImage: imageURLs,
+    );
+
+    return listing;
   }
 }
