58f399e983b114464c630dd7fc967962e9d370b3
raymyat <theingar98@gmail.com>
2021-04-05 18:14:56 +0800

    Fix profile image

--- project_crates/lib/backend/auth.dart
+++ project_crates/lib/backend/auth.dart
@@ -1,13 +1,21 @@
+import 'dart:io';
+
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_database/firebase_database.dart';
+import 'package:firebase_storage/firebase_storage.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_application_1/screens/authenticate/root.dart';
 import 'package:fluttertoast/fluttertoast.dart';
+import 'package:path_provider/path_provider.dart';
+import 'package:uuid/uuid.dart';
+import 'package:path/path.dart' as Path;
 
 final FirebaseAuth _auth = FirebaseAuth.instance;
 var _firebaseRef = FirebaseDatabase().reference().child('users');
-
+FirebaseStorage storage = FirebaseStorage.instance;
+var uuid = Uuid();
 // Display Login Successful/Unsuccessful
 displayToastMessage(String message, BuildContext context){
   Fluttertoast.showToast(msg: message);
@@ -23,16 +31,37 @@ Future<FirebaseUser> createUserWithEmailAndPassword(email, password, context) as
   return user;
 }
 
-void createUserDetails(userDB, username, email){
+void createUserDetails(userDB, username, email) async{
   FirebaseUser user = userDB;
+  var file = await imageToFile();
+  var url = await uploadProfilePic(file);
+
+
   _firebaseRef.child(user.uid).set({
     'userID':  user.uid, //Get from authentication db
     'username': username,
     'email': email,
     'isAdmin': false, //default false,
     'isBanned': '0', //default false
-    'imagePath': 'https://firebasestorage.googleapis.com/v0/b/test-firebase-c99c0.appspot.com/o/Profile%2Fdefault.png?alt=media&token=d414430d-92f0-4c06-a226-6ce88eadf8e5'//default path
+    'imagePath': url//default path
   });
+}
+ Future<File> imageToFile() async {
+  var bytes = await rootBundle.load('assets/icons/default.png');
+  String tempPath = (await getTemporaryDirectory()).path;
+  File file = File('$tempPath/default.png');
+  await file.writeAsBytes(
+  bytes.buffer.asUint8List(bytes.offsetInBytes, bytes.lengthInBytes));
+  return file;
+}
+Future<String> uploadProfilePic(File file) async {
+  String randomString = uuid.v4();
+  var storageReference = storage.ref().child('Profile/$randomString${Path.basename(file.path)}');
+  StorageUploadTask uploadTask = storageReference.putFile(file);
+  await uploadTask.onComplete;
+  print('File uploaded');
+  String url = await storageReference.getDownloadURL();
+  return url;
 }
 
 // For Sign In Page
