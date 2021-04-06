4937081d0d07851dcd10dc02c65671b91e28bda0
sAmanThaTan99 <38240412+sAmanThaTan99@users.noreply.github.com>
2021-04-06 16:35:53 +0800

    Completed Moderator_view test and added keys back to home page

---
+++ project_crates/integration_test/moderator_view_report_test.dart
@@ -0,0 +1,63 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_application_1/screens/moderator/completedReportListingPage.dart';
+import 'package:flutter_application_1/screens/moderator/oneReportListingCompleted.dart';
+import 'package:flutter_application_1/screens/moderator/reportListingPage.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:flutter_application_1/main.dart' as app;
+import 'package:integration_test/integration_test.dart';
+
+void main() {
+
+  group('Moderator View Report', () {
+    Future<void> login(WidgetTester tester) async {
+      app.main();
+      //sign_in.SignIn();
+      await tester.pumpAndSettle();
+      final emailFormField = find.byKey(Key('email'));
+      final passwordFormField = find.byKey(Key('password'));
+      final loginButton = find.byKey(Key('Login'));
+
+      await tester.tap(emailFormField);
+      await tester.enterText(emailFormField, 'admin@crates.com');
+      await tester.tap(passwordFormField);
+      await tester.enterText(passwordFormField, 'password');
+      await tester.pumpAndSettle();
+
+      await tester.tap(loginButton);
+      await tester.pumpAndSettle(Duration(seconds: 3));
+
+      // expect(find.byWidgetPredicate((widget) => widget is ReportListingPage ),findsOneWidget);
+    }
+
+    IntegrationTestWidgetsFlutterBinding.ensureInitialized();
+
+    testWidgets('View a completed listing', (WidgetTester tester) async {
+      await login(tester);
+      await tester.pumpAndSettle(Duration(seconds: 2));
+
+      final completedReportBtn = find.byIcon(Icons.note);
+      await tester.tap(completedReportBtn);
+      await tester.pumpAndSettle(Duration(seconds: 3));
+
+      expect(find.byWidgetPredicate((widget) => widget is CompletedReportListingPage),findsOneWidget);
+
+      final viewBtn = find.byKey(Key('CompletedListing')).first;
+      await tester.tap(viewBtn);
+      await tester.pumpAndSettle(Duration(seconds: 3));
+
+      expect(find.byWidgetPredicate((widget) => widget is oneReportCompleted ),findsOneWidget);
+
+    });
+
+    //need test after fixed
+    testWidgets('View a reported listing', (WidgetTester tester) async {
+      await login(tester);
+      await tester.pumpAndSettle(Duration(seconds: 2));
+
+      final viewBtn = find.byKey(Key('ReportListing')).first;
+      await tester.tap(viewBtn);
+      await tester.pumpAndSettle(Duration(seconds: 3));
+    });
+
+  });
+}
